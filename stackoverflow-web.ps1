5c1e91bd27a4a7f1dc4ea3cd331338bb783557c5
Gabriel <extofer@gmail.com>
2013-01-23 21:33:58 -0700

    added excluded the tag links

--- stackoverflow-web.ps1
+++ stackoverflow-web.ps1
@@ -6,7 +6,7 @@
 # stackoverflow search and select the 
 # Q&A to read text only
 
-$arg=$args[0]
+$arg= $args[0]
 cls
 $arg  = $arg -replace " ","+"
    Write-Host $step
@@ -14,14 +14,23 @@ $count = 0
 $search = Invoke-WebRequest "http://stackoverflow.com/search?q=$arg"
 
 # filters link results to "questions" only
-$results = $search.links | where href -like "*/questions/*" 
+$results = $search.links | where href -like "*/questions/*"  
+
+
 
 # list all results to key word searched
 foreach ($result in $results)
 {
   $count += 1 
-   Write-Host [$count]  -foregroundcolor green 
-   Write-Host $result.InnerText  
+    
+    # excluded the tag links
+    if ($result.href -notlike "*/questions/tagged*")
+    {
+        Write-Host [$count]  -foregroundcolor DarkMagenta
+        Write-Host $result.InnerText  
+    }
+
+
 }
 
 $getlink = Read-Host "Select the linkt you want to view" 
@@ -31,14 +40,16 @@ $getlink = Read-Host "Select the linkt you want to view"
 # gets content of the selected post
 $var = $results[$i].href
 
-Write-Host You selected $results[$i].InnerText uri $var -foregroundcolor green 
+Write-Host You selected $results[$i].InnerText uri $var  -foregroundcolor DarkMagenta 
 
 
 $data = Invoke-WebRequest "http://stackoverflow.com$var"
+Write-Host ******************************** Posted Question ********************************  -foregroundcolor DarkMagenta 
 # write the posted question
+$data.ParsedHtml.getElementsByTagName("div") |  Where "id" -match "question-header" | Select -ExpandProperty InnerText  
 $data.ParsedHtml.getElementsByTagName("div") |  Where "classname" -match "^question" | Select -ExpandProperty InnerText  
 
-Write-Host ******************************** Posted Answer ******************************** -foregroundcolor cyan 
+Write-Host ******************************** Posted Answer ********************************  -foregroundcolor DarkMagenta 
 
 # write the posted accepted-answer first
 $data.ParsedHtml.getElementsByTagName("div") |  Where "classname" -match "answer accepted-answer" | Select -ExpandProperty InnerText  
